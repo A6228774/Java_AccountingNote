63b1caeffe6623575d6aaf9ed7f2c8b92922df4d
A6228774 <A6228774@ulive.pccu.edu.tw>
2021-12-21 17:31:11 +0900

    ．新增和編輯分類（進行中）

--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/repository/CategoryRepository.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/repository/CategoryRepository.java
@@ -1,25 +1,40 @@
 package com.ubayKyu.accountingSystem.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ubayKyu.accountingSystem.entity.Category;
 
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-	
+public interface CategoryRepository extends JpaRepository<Category, Integer>,JpaSpecificationExecutor<Category>
+{
+
 	@Query(value = "SELECT * from Category WHERE UserID=?1", nativeQuery = true)
 	List<Category> findByUserIDSQL(String userid, Pageable pageable);
-	
+
 	@Query(value = "SELECT COUNT(*) as CNT from Category WHERE UserID=?1", nativeQuery = true)
 	Integer findTotalCategoryByUserIDSQL(String userid);
-	
+
 	@Query(value = "SELECT COUNT(*) as CNT from AccountingNote WHERE CategoryID=?1", nativeQuery = true)
 	Integer AccountingInCategory(Integer cid);
-	
+
 	@Query(value = "DELETE FROM Category WHERE CategoryID=?1", nativeQuery = true)
 	Category DeleteCategoryByCID(Integer cid);
+
+	@Query(value = "SELECT * FROM Category WHERE CategoryID=?1", nativeQuery = true)
+	Category findCategoryByCID(Integer cid);
+
+	@Query(value = "UPDATE Category SET Title=?1, Remarks=?2 WHERE CategoryID=?3", nativeQuery = true)
+	Category UpdateCategoryByCID(String titletxt, String remarkstxt, Integer cid);
+	
+	@Query(value = "SELECT Top 1 CategoryID FROM Category ORDER BY DESC", nativeQuery = true)
+	Integer lastCategory();
+	
+	@Query(value = "INSERT INTO Category (CategoryID, UserID, Title, Remarks, CreateDate) VALUES(?1, ?2, ?3, ?4, ?5)", nativeQuery = true)
+	Category CreateCategoryByCID(Integer newcid, String uidtxt, String titletxt, String remarkstxt, LocalDateTime ct);
 }
--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/service/CategoryService.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.ubayKyu.accountingSystem.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,5 +30,28 @@ public class CategoryService {
 
 	public Category DeleteCategoryByCID(Integer cid) {
 		return repository.DeleteCategoryByCID(cid);
+	}
+
+	public Category getCategoryByCID(Integer cid) {
+		return repository.findCategoryByCID(cid);
+	}
+
+	public void updateCategoryByCID(String titletxt, String remarkstxt, Integer cid) throws Exception {
+		if (titletxt.length() > 20) {
+			throw new Exception("標題字串長度大於20");
+		}
+		repository.UpdateCategoryByCID(titletxt, remarkstxt, cid);
+	}
+
+	public Integer getlastCID() {
+		return repository.lastCategory();
+	}
+
+	public void createCategoryByCID(Integer newcid, String uidtxt, String titletxt, String remarkstxt, LocalDateTime ct)
+			throws Exception {
+		if (titletxt.length() > 20) {
+			throw new Exception("標題字串長度大於20");
+		}
+		repository.CreateCategoryByCID(newcid, uidtxt, titletxt, remarkstxt, ct);
 	}
 }
